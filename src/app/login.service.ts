cdb5bda923638ee95a0a7fbb69fd00799a0f45de
Guillaume <g.belanger1996@gmail.com>
2021-11-17 23:12:07 -0500

    fix: typo

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, from, Observable, of } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import { BehaviorSubject, from, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { User } from './models/user';
 
 
