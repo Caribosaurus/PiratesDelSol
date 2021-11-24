9db642c214fa3b5e457246a8f0b469fe4c1cfc0b
Caribosaurus <guillaume.belanger.6@ens.etsmtl.ca>
2021-11-23 19:09:22 -0500

    Add icons

--- src/app/offering/offering.component.ts
+++ src/app/offering/offering.component.ts
@@ -9,26 +9,32 @@ export class OfferingComponent implements OnInit {
   offerings: Array<any> = [
     {
       title: "Monthly income Treasure Chest",
+      icon: "/assets/icons/Treasure_chest.png",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet ultricies dui, et posuere lacus. Vestibulum accumsan justo eget justo rhoncus convallis."
     },
     {
       title: "Token Reward System",
+      icon: "/assets/icons/Token.png",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet ultricies dui, et posuere lacus. Vestibulum accumsan justo eget justo rhoncus convallis."
     },
     {
       title: "Reduce supply / Increase scarcity mechanism",
+      icon: "/assets/icons/Chart.png",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet ultricies dui, et posuere lacus. Vestibulum accumsan justo eget justo rhoncus convallis."
     },
     {
       title: "Handmade Artwork with rarity traits",
+      icon: "/assets/icons/Ranks.png",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet ultricies dui, et posuere lacus. Vestibulum accumsan justo eget justo rhoncus convallis."
     },
     {
       title: "Gamble your NFT's mini games",
+      icon: "/assets/icons/Dice.png",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet ultricies dui, et posuere lacus. Vestibulum accumsan justo eget justo rhoncus convallis."
     },
     {
       title: "Community charitable donation",
+      icon: "/assets/icons/Charity.png",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet ultricies dui, et posuere lacus. Vestibulum accumsan justo eget justo rhoncus convallis."
     },
 
