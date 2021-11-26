554d787c9531d0ca2668ff94fa83c7e3ff821ac0
Caribosaurus <guillaume.belanger.6@ens.etsmtl.ca>
2021-11-25 19:35:10 -0500

    Updated roadmap

--- src/app/roadmap/roadmap.component.ts
+++ src/app/roadmap/roadmap.component.ts
@@ -9,73 +9,73 @@ export class RoadmapComponent implements OnInit {
   roadmapItems: Array<any> = [
     {
       title:'Placeholder', 
-      image:'/assets/img/zoltan-tasi.jpg', 
+      image:'/assets/img/Roadmap/Roadmap_1.png', 
       description: 'Ut eu tempor lectus. Nulla odio nisl, dictum id facilisis et, luctus varius nisi. Aliquam eu nisl sit amet diam convallis commodo.',
       date: "January 2021"
     },
     {
       title:'Placeholder', 
-      image:'/assets/img/zoltan-tasi.jpg', 
+      image:'/assets/img/Roadmap/Roadmap_2.png', 
       description: 'Ut eu tempor lectus. Nulla odio nisl, dictum id facilisis et, luctus varius nisi. Aliquam eu nisl sit amet diam convallis commodo.',
       date: "January 2021"
     },
     {
       title:'Placeholder', 
-      image:'/assets/img/zoltan-tasi.jpg', 
+      image:'/assets/img/Roadmap/Roadmap_3.png', 
       description: 'Ut eu tempor lectus. Nulla odio nisl, dictum id facilisis et, luctus varius nisi. Aliquam eu nisl sit amet diam convallis commodo.',
       date: "January 2021"
     },
     {
       title:'Placeholder', 
-      image:'/assets/img/zoltan-tasi.jpg', 
+      image:'/assets/img/Roadmap/Roadmap_4.png', 
       description: 'Ut eu tempor lectus. Nulla odio nisl, dictum id facilisis et, luctus varius nisi. Aliquam eu nisl sit amet diam convallis commodo.',
       date: "January 2021"
     },
     {
       title:'Placeholder', 
-      image:'/assets/img/zoltan-tasi.jpg', 
+      image:'/assets/img/Roadmap/Roadmap_5.png', 
       description: 'Ut eu tempor lectus. Nulla odio nisl, dictum id facilisis et, luctus varius nisi. Aliquam eu nisl sit amet diam convallis commodo.',
       date: "January 2021"
     },
     {
       title:'Placeholder', 
-      image:'/assets/img/zoltan-tasi.jpg', 
+      image:'/assets/img/Roadmap/Roadmap_6.png', 
       description: 'Ut eu tempor lectus. Nulla odio nisl, dictum id facilisis et, luctus varius nisi. Aliquam eu nisl sit amet diam convallis commodo.',
       date: "January 2021"
     },
     {
       title:'Placeholder', 
-      image:'/assets/img/zoltan-tasi.jpg', 
+      image:'/assets/img/Roadmap/Roadmap_7.png', 
       description: 'Ut eu tempor lectus. Nulla odio nisl, dictum id facilisis et, luctus varius nisi. Aliquam eu nisl sit amet diam convallis commodo.',
       date: "January 2021"
     },
     {
       title:'Placeholder', 
-      image:'/assets/img/zoltan-tasi.jpg', 
+      image:'/assets/img/Roadmap/Roadmap_8.png', 
       description: 'Ut eu tempor lectus. Nulla odio nisl, dictum id facilisis et, luctus varius nisi. Aliquam eu nisl sit amet diam convallis commodo.',
       date: "January 2021"
     },
     {
       title:'Placeholder', 
-      image:'/assets/img/zoltan-tasi.jpg', 
+      image:'/assets/img/Roadmap/Roadmap_9.png', 
       description: 'Ut eu tempor lectus. Nulla odio nisl, dictum id facilisis et, luctus varius nisi. Aliquam eu nisl sit amet diam convallis commodo.',
       date: "January 2021"
     },
     {
       title:'Placeholder', 
-      image:'/assets/img/zoltan-tasi.jpg', 
+      image:'/assets/img/Roadmap/Roadmap_10.png', 
       description: 'Ut eu tempor lectus. Nulla odio nisl, dictum id facilisis et, luctus varius nisi. Aliquam eu nisl sit amet diam convallis commodo.',
       date: "January 2021"
     },
     {
       title:'Placeholder', 
-      image:'/assets/img/zoltan-tasi.jpg', 
+      image:'/assets/img/Roadmap/Roadmap_11.png', 
       description: 'Ut eu tempor lectus. Nulla odio nisl, dictum id facilisis et, luctus varius nisi. Aliquam eu nisl sit amet diam convallis commodo.',
       date: "January 2021"
     },
     {
       title:'Placeholder', 
-      image:'/assets/img/zoltan-tasi.jpg', 
+      image:'/assets/img/Roadmap/Roadmap_12.png', 
       description: 'Ut eu tempor lectus. Nulla odio nisl, dictum id facilisis et, luctus varius nisi. Aliquam eu nisl sit amet diam convallis commodo.',
       date: "January 2021"
     },
