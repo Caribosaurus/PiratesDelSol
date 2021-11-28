8c48be41da962b31624798ff126a81207ceb33c7
Caribosaurus <guillaume.belanger.6@ens.etsmtl.ca>
2021-11-27 23:28:08 -0500

    automated cloudfront invalidation for dev

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   env: 'prod',
-  discordCallbackUrl:'https://piratesdelsol.com'
+  discordCallbackUrl:'https://piratesdelsol.com',
+  apiUrl: "https://api.piratesdelsol.com"
 };
--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
     env: 'uat',
-    discordCallbackUrl:'https://dev.piratesdelsol.com'
+    discordCallbackUrl:'https://dev.piratesdelsol.com',
+    apiUrl: "https://api.piratesdelsol.com"
   };
   
